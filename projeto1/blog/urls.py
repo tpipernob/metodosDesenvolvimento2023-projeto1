149bcb1644327c5030dd25f1aec829379d1ffd05
Tiago Piperno Bonetti <tiagopiperno@gmail.com>
2023-10-31 15:21:44 -0300

    2 bimestre aula 01

--- projeto1/blog/urls.py
+++ projeto1/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name="home"),
-    path('post/<int:post_id>', views.post_detalhe, name="post-detalhe"),
+    path('', views.post_list.as_view(), name="home"),
+    path('post/<int:pk>', views.post_detalhe.as_view(), name="post-detalhe"),
 
 ]
--- projeto1/blog/views.py
+++ projeto1/blog/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from .models import Post
+from django.views import generic
+
+class post_list(generic.ListView):
+    modelo = Post
+    template_name = 'blog/home.html'
+
+    def get_queryset(self):
+        return Post.objects.all().order_by('-data')[:3]
 
 
-def post_list(request):
-    posts = Post.objects.all()
-    
-    return render(request, 'blog/home.html', {'posts' : posts})
-
-
-def post_detalhe(request, post_id):
-    post = Post.objects.get(pk=post_id)
-
-    return render(request, 'blog/post_detalhe.html', {'post' : post})
+class post_detalhe(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detalhe.html'
